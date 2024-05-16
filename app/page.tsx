069c99438fe96387f2e86513b76775a9c0dcceec
Ryan <rom@ualberta.ca>
2024-05-15 23:46:36 -0600

    Connect the client to the server

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { ChromePicker } from 'react-color'
 import { io } from 'socket.io-client'
 import { drawLine } from '@/utils/drawLine'
 
-const socket = io('http://localhost:3001')
+const socket = io('https://the-board-server.vercel.app/')
 
 interface pageProps {}
 
